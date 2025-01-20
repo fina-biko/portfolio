7b3632c2dc5576a1e150541e9b428deb471650aa
fina-biko <finarheiner@gmail.com>
2025-01-20 21:04:15 +0300

    Updated the Header.jsx to include small screen adjustment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 const Header = () => {
   return (
-      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 w-full items-center md:items-end justify-center md:justify-end">
+      <div
+          className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full items-start md:items-end justify-start md:justify-end">
           <div>
               <a
                   href="/src/components/About.jsx"
-                  className="text-white hover:underline hover:text-white mb-4"
+                  className="text-white hover:underline mb-4"
               >
                   About
               </a>
@@ -12,7 +13,7 @@ const Header = () => {
           <div>
               <a
                   href="/src/components/Projects.jsx"
-                  className="text-white hover:underline hover:text-white mb-4"
+                  className="text-white hover:underline mb-4"
               >
                   Projects
               </a>
@@ -20,20 +21,21 @@ const Header = () => {
           <div>
               <a
                   href="/src/components/Resume.jsx"
-                  className="text-white hover:underline hover:text-white mb-4"
+                  className="text-white hover:underline mb-4"
               >
                   Resume
               </a>
           </div>
           <div>
               <a
-                  href="/src/components/Resume.jsx"
-                  className="text-white hover:underline hover:text-white mb-4"
+                  href="/src/components/Contact.jsx"
+                  className="text-white hover:underline mb-4"
               >
                   Contact
               </a>
           </div>
       </div>
+
   );
 };
 
