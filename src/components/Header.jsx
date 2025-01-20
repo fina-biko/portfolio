443eaa884fa7620d5377403ddea8725b5448fbc0
fina-biko <finarheiner@gmail.com>
2025-01-20 21:13:44 +0300

    Updated the Header.jsx to include small screen adjustment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,35 @@
 const Header = () => {
   return (
       <div
-          className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full items-start md:items-end justify-start md:justify-end">
-          <div>
+          className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full items-start md:items-end justify-start md:justify-end overflow-x-auto">
+          <div className="mb-4 md:mb-0">
               <a
                   href="/src/components/About.jsx"
-                  className="text-white hover:underline mb-4"
+                  className="text-white hover:underline"
               >
                   About
               </a>
           </div>
-          <div>
+          <div className="mb-4 md:mb-0">
               <a
                   href="/src/components/Projects.jsx"
-                  className="text-white hover:underline mb-4"
+                  className="text-white hover:underline"
               >
                   Projects
               </a>
           </div>
-          <div>
+          <div className="mb-4 md:mb-0">
               <a
                   href="/src/components/Resume.jsx"
-                  className="text-white hover:underline mb-4"
+                  className="text-white hover:underline"
               >
                   Resume
               </a>
           </div>
-          <div>
+          <div className="mb-4 md:mb-0">
               <a
                   href="/src/components/Contact.jsx"
-                  className="text-white hover:underline mb-4"
+                  className="text-white hover:underline"
               >
                   Contact
               </a>
